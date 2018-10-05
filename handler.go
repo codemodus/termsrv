d8e60b364d970051cb70d4d7a21bec5322646452
Daved <daved@codemodus.com>
2018-10-05 14:25:44 -0700

    Add access error logging.

--- handler.go
+++ handler.go
@@ -10,12 +10,12 @@ func wsHandler(ug *websocket.Upgrader, mq *msgq) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		cx, err := ug.Upgrade(w, r, nil)
 		if err != nil {
-			logError("cannot upgrade connection", err)
+			logAcsError("cannot upgrade connection", err)
 			return
 		}
 		defer func() {
 			if err = cx.Close(); err != nil {
-				logError("cannot close connection", err)
+				logAcsError("cannot close connection", err)
 			}
 		}()
 
@@ -26,21 +26,21 @@ func wsHandler(ug *websocket.Upgrader, mq *msgq) http.HandlerFunc {
 
 		c, err := mq.attach(done)
 		if err != nil {
-			logError("cannot attach to message queue", err)
+			logAcsError("cannot attach to message queue", err)
 			return
 		}
 
 		go func() {
 			for v := range c {
 				if werr := cx.WriteMessage(websocket.TextMessage, v); werr != nil {
-					logError("cannot write to connection", werr)
+					logAcsError("cannot write to connection", werr)
 				}
 			}
 		}()
 
 		if _, _, rerr := cx.ReadMessage(); rerr != nil {
 			if !websocket.IsCloseError(rerr, websocket.CloseNormalClosure, websocket.CloseNoStatusReceived) {
-				logError("connection close error", rerr)
+				logAcsError("connection close error", rerr)
 			}
 		}
 	}
